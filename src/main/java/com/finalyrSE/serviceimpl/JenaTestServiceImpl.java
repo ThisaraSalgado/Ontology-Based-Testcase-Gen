3a1316aa4ce882e81f8851c8cd2b6b47b30b1b62
Thisara Salgado <mthisarays@gmail.com>
2017-12-11 12:31:53 +0530

    Started save and generate with testcases

--- src/main/java/com/finalyrSE/serviceimpl/JenaTestServiceImpl.java
+++ src/main/java/com/finalyrSE/serviceimpl/JenaTestServiceImpl.java
@@ -78,4 +78,59 @@ public class JenaTestServiceImpl implements JenaTestService {
 		return null;
 	}
 
+	@Override
+	public String jenaWithParam(String actor, String action, String object) {
+		String filename = "C:/Users/ThisaraPC/common12-Copy.rdf";
+		System.out.println("file loaded");
+		// Create an empty model
+		Model model = ModelFactory.createDefaultModel();
+				
+		// Use the FileManager to find the input file
+		InputStream in = FileManager.get().open(filename);
+
+		if (in == null)
+			throw new IllegalArgumentException("File: "+filename+" not found");
+
+		// Read the RDF/XML file
+		model.read(in, null);
+		String act = "Admin";
+		String par = "Add";
+		//String obj = "Group";
+		ParameterizedSparqlString pss = new ParameterizedSparqlString();
+		
+		//pss.setLiteral(par, "Create");
+		// List all the resources with the property "vcard:FN"
+		pss.setCommandText("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+						"PREFIX owl: <http://www.w3.org/2002/07/owl#>"+
+						"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+						"PREFIX test: <http://www.semanticweb.org/prabhavi/ontologies/2017/9/untitled-ontology-53#>" +
+						"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>" +
+						"PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
+						"SELECT ?x " +
+						"WHERE {" +
+						" test:"+actor+" test:hasAction test:"+par+" ."+
+						" test:"+par+" test:hasObject ?x ."+
+						"}");
+		
+		String queryString = pss.toString();
+		System.out.println(queryString);
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qexec = QueryExecutionFactory.create(query, model) ;
+		ResultSet results = qexec.execSelect() ;
+		
+		while (results.hasNext())
+		{
+			System.out.println("in file 1");
+			QuerySolution binding = results.nextSolution();
+			Resource subj = (Resource) binding.get("x");
+		    String resultString = subj.getURI();
+		    String result;
+		    //get result as string without URI prefix, but different approach to get in from the query execution
+		    System.out.println(resultString.substring(resultString.lastIndexOf("#") +1));
+		    result = resultString.substring(resultString.lastIndexOf("#") +1);
+		    
+		}
+		return null;
+	}
+
 }
